9863820aea8bf22b6f510e18fa6aa8f80a4504c2
Andy Barron <AndrewLBarron@gmail.com>
2019-06-03 12:48:56 -0700

    publish

---
+++ docs/precache-manifest.72f4da041324cde6c28e9cbce381c19f.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb4790dac7cab2e2412330c886622a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d4ad608a8915f7aad9e",
+    "url": "/static/js/main.4017d999.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
